d97214c560738f2b3851a03b640b9815668da56e
spo <sebastien.porquet@atomicweb.fr>
2024-05-03 17:44:30 +0200

    v1.1.1.

--- source/conf.py
+++ source/conf.py
@@ -22,8 +22,8 @@ copyright = '2024, PORQUET Sébastien'
 author = 'PORQUET Sébastien'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.0'
-version = '1.1.0'
+release = '1.1.1'
+version = '1.1.1'
 
 # -- General configuration ---------------------------------------------------
 
